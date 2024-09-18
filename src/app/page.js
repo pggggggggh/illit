084d880e4f98582cb0857c878ded2b622fa37d25
pggggggggh <pggggggggh@yonsei.ac.kr>
2024-09-19 01:09:16 +0900

    form

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,3 @@
-'use client';
-
 import Image from "next/image";
 import {
     Card,
@@ -14,9 +12,6 @@ import {Masonry} from "@mui/lab";
 import UploadModal from "@/components/ModalComponent";
 
 export default function Home() {
-    const theme = useTheme();
-    const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
-    const columns = isSmallScreen ? 1 : 3;
     const itemData = [
         {
             src: "https://assets.teenvogue.com/photos/66159ed9db21f3a8c3c35fd1/master/w_1600,c_limit/ILLIT_first%20week_1.jpg",
@@ -34,7 +29,7 @@ export default function Home() {
 
     return (
         <Container maxWidth="lg" sx={{p: 4}}>
-            <Masonry columns={columns} spacing={2}>
+            <Masonry columns={{xs: 1, sm: 3}} spacing={2}>
                 <Card
                     key={100}
                     sx={{
@@ -102,15 +97,17 @@ export default function Home() {
                         }}
                     >
                         <CardMedia>
-                            <Image
-                                className="image"
-                                src={item.src}
-                                alt={""}
-                                layout="responsive"
-                                width={item.width}
-                                height={item.height}
-                                style={{transition: "transform 0.3s ease-in-out"}}
-                            />
+                            <a href={item.src} download>
+                                <Image
+                                    className="image"
+                                    src={item.src}
+                                    alt={""}
+                                    layout="responsive"
+                                    width={item.width}
+                                    height={item.height}
+                                    style={{transition: "transform 0.3s ease-in-out"}}
+                                />
+                            </a>
                         </CardMedia>
                     </Card>
                 ))}
