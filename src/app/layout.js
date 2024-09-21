d711e835fd69d10c16aace752f69f39e11e021f5
pggggggggh <pggggggggh@yonsei.ac.kr>
2024-09-21 19:02:35 +0900

    fin

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
         <html lang="en">
         <head>
             <meta name="naver-site-verification" content="04d2b338a878617a1cf74ccc20e41b4a272cf8e7"/>
+            <link rel="icon" href="/favicon.ico"/>
         </head>
         <body>
 
